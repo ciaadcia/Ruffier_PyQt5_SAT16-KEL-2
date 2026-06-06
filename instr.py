46a6c493b6f9da9e145a6a50a8aa734ded75450f
FleurDeNoir <serpentdemare@gmail.com>
2026-06-06 17:22:13 +0700

    Update instr.py

--- instr.py
+++ instr.py
@@ -1,1 +1,19 @@
-
+#tambahkan
+txt_hintage = '0'
+txt_test1 = 'Lie on your back and take your pulse for 15 seconds. Click the "Start first test" button to start the timer.\n' \
+'Write down the result in the appropriate field.'
+txt_test2 = 'Perform 30 squats in 45 seconds. To do this, click the "Start doing squats" button\nto start the squat counter.'
+txt_test3 = 'Lie on your back and take your pulse for the first 15 seconds of the minute, then for the last 15 seconds of the ' \
+'minute.\nPress the "Start final test" button to start the timer.nThe second that should be measured are indicated in green ' \
+'and the minutes that should not be measured are indicated in black. Write down the result in the appropriate fields.'
+txt_hinttest1 = '0'
+txt_hinttest2 = '0'
+txt_hinttest3 = '0'
+txt_starttest1 = 'Start the first test'
+txt_starttest2 = 'Start doing squats'
+txt_starttest3 = 'Start the final test'
+txt_timer = ''
+txt_age = 'Full years:'
+txt_finalwin = 'Results'
+txt_index = 'Roufier Index: '
+txt_workheart = 'Cardiac performance: '
